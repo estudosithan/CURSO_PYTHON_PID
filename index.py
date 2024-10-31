e5b6943d5b4a3dc36136a3aaecdd9f60b6b3a67a
Íthan P. Amaral <ithanamaral@gmail.com>
2024-10-31 10:11:26 -0300

    Subindo Arquivos para o github

---
+++ index.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+
+x = 5
+y = 3.14
+z = " Hello "
+w = True
+
+print ( type ( x ) ) # Saida : <class ’int ’>
+print ( type ( y ) ) # Saida : <class ’float ’>
+print ( type ( z ) ) # Saida : <class ’str ’>
+print ( type ( w ) ) # Saida : <class ’bool ’>
+
+
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + "!")
+
+
+
+
+
+
+
+
+
+
+
+
