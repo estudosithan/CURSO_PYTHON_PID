1b81c7dd16ff74a359c841a8614f3d2ec5292e5e
Íthan P. Amaral <ithanamaral@gmail.com>
2025-03-15 17:33:02 -0300

    mudando nome da branch

--- P.O.O/transform_json.py
+++ P.O.O/transform_json.py
@@ -12,7 +12,7 @@ P2 = Pessoa("Halisson", 21)
 P3 = Pessoa("Emerson", 19)
 P4 = Pessoa("Camille", 23)
 bd = [vars(P1), vars(P2), vars(P3), vars(P4)] #transforma os objetos em dicionarios
-#cria uma lista por que é uma coleção de informações
+#cria uma lista por que é uma coleção de informações.
 
 with open(CAMINHO_ARQUIVO, 'w') as arquivo:
     json.dump(bd, arquivo, ensure_ascii=False, indent=2)
